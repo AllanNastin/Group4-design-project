f6047059e5243ee23869008de473cd9d17dd9c96
AllanNastin <nastina@tcd.ie>
2025-02-17 21:36:00 +0000

    update cors policy

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import json
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000", "https://gdp4.sprinty.tech"])
 
 @app.route("/")
 def main():
     return jsonify({"data": "hello world"})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
