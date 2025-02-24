f9b49388122d254efa2204fd79489316f72db49a
Daniel Padmore <DanielPadmore02@gmail.com>
2025-02-24 10:45:08 +0000

    added apiUrl back to ListingsParser

--- frontend/src/components/ListingsParser.js
+++ frontend/src/components/ListingsParser.js
@@ -10,7 +10,10 @@ const ListingsParser = () => {
     const navigate = useNavigate();
     const { state } = useLocation();
 
-    // Fetch listings data from JSON file
+    const apiUrl = process.env.REACT_APP_API_URL;
+
+
+    // Fetch listings data from JSON file   const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000';
     useEffect(() => {
         /*
         fetch("/Sample.json")
@@ -38,7 +41,7 @@ const ListingsParser = () => {
                     setLoading(false);
                     return;
                 }
-                const response = await axios.get('https://gdp4back.sprinty.tech/getListings', {
+                const response = await axios.get(`${apiUrl}/getListings`, {
                     // add request params
                     params: {
                         type : state.type,
