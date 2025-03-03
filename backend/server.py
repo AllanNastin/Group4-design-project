40de2a401f22233b995f64904801234a320632b4
AllanNastin <nastina@tcd.ie>
2025-03-03 20:37:55 +0000

    update env getting for api

--- backend/server.py
+++ backend/server.py
@@ -4,17 +4,12 @@ import json
 from maps import get_distance_and_times
 import scrap_daft
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 google_api_key = os.getenv("GOOGLE_API")
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000", "https://gdp4.sprinty.tech", "https://dev-gdp4.sprinty.tech"])
-
-# Pre-scrape listings on startup
-# try:
-#     listings_data = scrap_daft.daft_scraper_json(0, 1)  # Scrape the first page
-# except Exception as e:
-#     print(f"Error during scraping: {e}")
-#     listings_data = json.dumps({"error": "Failed to scrape listings"})  # Error as JSON
 
 @app.route("/")
 def main():
@@ -39,6 +34,12 @@ def getListings():
 
     if listing_type is "sale":
         listings_data = scrap_daft.daft_scraper_json(0, 1, eircode=location, listing_type=listing_type)
+        # listings = json.loads(listings_data)
+        # print(f"Listings: {listings}")  # Debug print
+        # for listing in listings['listings']:
+        #     print(f"Processing listing: {listing}")  # Debug print
+        #     listing["distance"], listing["car"], listing["walk"] = get_distance_and_times(location, listing["address"], google_api_key)
+        # listings_data = json.dumps(listings)
     else:
         listings_data = scrap_daft.daft_scraper_json(0, 1, listing_type=listing_type)
     return Response(listings_data, mimetype='application/json') # Return the pre-scraped JSON
