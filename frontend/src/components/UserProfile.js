8328e01984fd08fca21aebc4b67f1cc5aad7f41e
Dongyun Tang <dongyuntom@gmail.com>
2025-03-31 22:33:49 +0100

    target “Favorites” to SavedListing page

--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -31,7 +31,7 @@ const UserProfile = () => {
     };
 
     const handleFavourites = () => {
-        window.location.href = "/favourites";
+        window.location.href = "/saved-listings";
     };
 
     useEffect(() => {
